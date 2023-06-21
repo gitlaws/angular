ae25ebcd64ea932bca0d990d07568070a7c17935
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-06-21 19:38:05 -0400

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentDocumentationComponent } from './views/component-documentation/component-documentation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentDocumentationComponent
   ],
   imports: [
     BrowserModule
