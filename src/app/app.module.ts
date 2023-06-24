c01004f04b9851efd86cd3a35fa3c61b86c383ee
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-06-24 15:29:14 -0400

    added routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from "@angular/router";
 import { AppComponent } from './app.component';
+import { APP_ROUTES } from "./app.routes";
 import { ComponentDocumentationComponent } from './views/component-documentation/component-documentation.component';
 import { TopOfPageComponent } from './components/top-of-page/top-of-page.component';
 
